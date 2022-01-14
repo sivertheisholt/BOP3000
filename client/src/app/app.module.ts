440d23f5778785f28e3e9c4499cc248f851b23dc
sivertheisholt <sivert.b.heisholt@protonmail.com>
2022-01-14 15:51:41 +0100

    Shared modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { StartComponent } from './start/start.component';
-import {FormsModule} from '@angular/forms'
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,9 +21,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    FormsModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
