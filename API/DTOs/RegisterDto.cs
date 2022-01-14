98f7f4c1d0ab4cc7e4049c031bc151cc6dc050e1
sivertheisholt <sivert.b.heisholt@protonmail.com>
2022-01-14 17:31:24 +0100

    Error Handling Middleware

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -12,6 +12,7 @@ namespace API.DTOs
         public string Username { get; set; }
 
         [Required]
+        [StringLength(16, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
