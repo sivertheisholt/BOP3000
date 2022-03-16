35ecf4f7a09c07ff229faa754f69ed22167071d1
dyb0 <dybevik2@gmail.com>
2022-03-16 22:14:33 +0100

    connect to steam form

--- client/src/app/components/settings/connections/connections.component.ts
+++ client/src/app/components/settings/connections/connections.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-connections',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connections.component.css']
 })
 export class ConnectionsComponent implements OnInit {
+  connectToSteamForm!: FormGroup;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.connectToSteamForm = new FormGroup({
+      Name: new FormControl("Steam"),
+      ReturnUrl: new FormControl("/")
+    });
+  }
+
+  onSubmit(){
+    console.log(this.connectToSteamForm.value);
+    this.http.post('URL HER', this.connectToSteamForm.value).pipe(
+      map((response) => {
+        console.log(response);
+      })
+    ).subscribe(
+      response => {
+        console.log(response);
+      }
+    )
   }
 
 }
