54447954448fff63fa302b54fe47f4bca38e6eba
sivertheisholt <sivert.b.heisholt@protonmail.com>
2022-03-29 15:04:47 +0200

    Lobby: Sending uid on accept/decline

--- API/SignalR/LobbyHub.cs
+++ API/SignalR/LobbyHub.cs
@@ -140,13 +140,13 @@ namespace API.SignalR
         {
             var uid = Context.User.GetUserId();
             await _lobbyTracker.AcceptReady(lobbyId, uid);
-            await Clients.Group($"lobby_{lobbyId.ToString()}").SendAsync("MemberAccepted");
+            await Clients.Group($"lobby_{lobbyId.ToString()}").SendAsync("MemberAccepted", uid);
         }
         public async Task Decline(int lobbyId)
         {
             var uid = Context.User.GetUserId();
             await _lobbyTracker.DeclineReady(lobbyId, uid);
-            await Clients.Group($"lobby_{lobbyId.ToString()}").SendAsync("MemberDeclined");
+            await Clients.Group($"lobby_{lobbyId.ToString()}").SendAsync("MemberDeclined", uid);
         }
 
         public async Task GetQueueMembers(int lobbyId)
